325a09813e484b3274ca82ed71bd452b76550b47
jdowning100 <jdowning@utexas.edu>
2025-02-17 12:01:23 -0600

    update to use permit

--- components/lib/interactions/deploy.ts
+++ components/lib/interactions/deploy.ts
@@ -2,11 +2,21 @@ import { quais } from 'quais';
 import ERC20 from '../contracts/erc20/ERC20.json';
 import ERC721JsonURI from '../contracts/erc721/JsonURI/ERC721.json';
 import ERC721URI from '../contracts/erc721/URI/ERC721.json';
+import * as CBOR from 'cbor-x'
+import bs58 from 'bs58'
+import { splitAuxdata } from '@ethereum-sourcify/bytecode-utils';
+import { arrayify } from '@ethersproject/bytes'
 
 export const deployToken = async ({ name, symbol, supply, web3Provider }: DeployERC20Props) => {
   try {
     const signer = await web3Provider.getSigner();
-    const ERC20contract = new quais.ContractFactory(ERC20.abi, ERC20.bytecode, signer);
+    let metadata = decodeMultipleMetadataSections(ERC20.bytecode);
+    console.log(metadata)
+    let ipfsHash = "0000000000000000000000000000000000000000000000"
+    if (metadata.length > 0) {
+      ipfsHash = metadata[0].ipfs
+    }
+    const ERC20contract = new quais.ContractFactory(ERC20.abi, ERC20.bytecode, signer, ipfsHash);
     console.log('Deploying ERC20 token...', ERC20contract);
     const erc20 = await ERC20contract.deploy(name, symbol, quais.parseUnits(supply));
     await erc20.waitForDeployment();
@@ -34,4 +44,39 @@ export const deployNFT = async ({ name, symbol, extension, uri, collectionSize, 
   } catch (err) {
     return Promise.reject(err);
   }
+};
+
+const decodeMultipleMetadataSections = (bytecode: string) => {
+  if (!bytecode || bytecode.length === 0) {
+      throw new Error("Bytecode cannot be empty");
+  }
+  bytecode = ensureHexPrefix(bytecode);
+
+  const metadataSections = [];
+  let remainingBytecode = bytecode;
+
+  while (remainingBytecode.length > 0) {
+    try {
+      const [executionBytecode, auxdata] = splitAuxdata(remainingBytecode);
+
+      if (auxdata) {
+        const decodedMetadata = CBOR.decode(arrayify(`0x${auxdata}`));
+        metadataSections.push(decodedMetadata);
+        remainingBytecode = executionBytecode;
+      } else {
+        break;
+      }
+    } catch (error) {
+      console.log(error)
+      break;
+    }
+  }
+
+  return metadataSections.map((metadata) => ({
+    ...metadata,
+    ipfs: metadata.ipfs ? bs58.encode(metadata.ipfs) : undefined,
+  }));
+};
+const ensureHexPrefix = (bytecode: string) => {
+  return bytecode.startsWith('0x') ? bytecode : `0x${bytecode}`;
 };
